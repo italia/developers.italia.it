3debd9de4213b7e4cbae40c88f627e7d9a1db348
IceOnFire <antony.mistretta@gmail.com>
2017-03-08 00:20:58 +0100

    Add and apply stylelint to the project

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": [
+    "stylelint-selector-bem-pattern"
+  ],
+  "rules": {
+    "plugin/selector-bem-pattern": {
+      "preset": "suit"
+    },
+    "at-rule-empty-line-before": null,
+  },
+  "extends": "stylelint-config-suitcss"
+}
