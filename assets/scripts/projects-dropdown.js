aaa65ff2cf420420f74ee4bf355ebd7079a80375
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-03-16 01:05:57 +0100

    fix search box

--- assets/scripts/projects-dropdown.js
+++ assets/scripts/projects-dropdown.js
@@ -1,9 +1,18 @@
-$(function() {
-  $('#cerca').on('keyup', function(event) {
-    var text = event.target.value.toLowerCase()
-    $('#projects > ul > li').each(function(index, elem) {
-      var project = $(elem).find('span').html().toLowerCase()
-      $(elem).toggleClass('u-hiddenVisually', project.indexOf(text) < 0)
+$(document).ready(function() {
+  $(function() {
+    $('#cerca').on('keyup', function(event) {
+      var text = event.target.value.toLowerCase()
+      $('#projects > ul > li').each(function(index, elem) {
+        var project = $(elem).find('span').html().toLowerCase()
+        if (project.indexOf(text) < 0) {
+          $(elem).addClass('u-hidden')
+          $(elem).attr('tabindex', '-1')
+        }
+        else {
+          $(elem).removeClass('u-hidden')
+          $(elem).attr('tabindex', '0')
+        }
+      })
     })
   })
 })
