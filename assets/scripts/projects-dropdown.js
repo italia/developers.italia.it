34fa8fdf9a2d5da18cbe0154eadfa73c1a84383a
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2017-03-23 08:44:35 +0100

    fix html / js + link strumenti

--- assets/scripts/projects-dropdown.js
+++ assets/scripts/projects-dropdown.js
@@ -7,16 +7,22 @@ $(document).ready(function() {
         return false
       }
     })
+    $('#cerca').on('keydown', function(event) {
+      if (event.which === 9 && $(this).is(':focus')) {
+        $(this).blur()
+        return true
+      }
+    })
     $('#cerca').on('keyup', function(event) {
       var text = event.target.value.toLowerCase()
       $('#projects > ul > li').each(function(index, elem) {
         var project = $(elem).find('span').html().toLowerCase()
         if (project.indexOf(text) < 0) {
-          $(elem).addClass('u-hiddenVisually')
-          $(elem).attr('tabindex', '-1')
+          $(elem)
+            .addClass('u-hiddenVisually')
         } else {
-          $(elem).removeClass('u-hiddenVisually')
-          $(elem).attr('tabindex', '0')
+          $(elem)
+            .removeClass('u-hiddenVisually')
         }
       })
     })
