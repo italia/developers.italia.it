741e5ef47830ef3da447dcc0bbdc0df7d45aa19b
Vito Falco <vitofalco@gmail.com>
2017-09-20 19:16:10 +0200

    Aggiunta di pagina con elenco issue (#66)

---
+++ _pages/it/cosa-fare.md
@@ -0,0 +1,12 @@
+---
+title: Cosa fare
+subtitle: Le issues aperte nei repo di Developers Italia
+lang: it
+layout: github-issues
+---
+
+Hai voglia di aiutare ma non sai da dove cominciare? Tutti gli sviluppi che facciamo sono su GitHub [@italia](https://github.com/italia "Italia su Github"), ma i progetti sono tanti e utilizzano tecnologie diverse. In questa pagina, raccogliamo tutte le issue aperte di tutti i progetti, e potrai quindi cercare un task con più facilità. Utilizza i filtri per orientarti più facilmente e trovare qualcosa a te più affine.
+
+
+{:.u-textWeight-400 .u-margin-top-l }
+## Le issue aperte
---
+++ assets/js-perpage/github-issues.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+        var datatables_language_url = {
+            "en" : "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/English.json",
+            "it" : "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Italian.json"
+        };
+        var current_language = $('html').attr('lang');
+        var $issues_table_clear = $('#issues_table_clear');
+        var $github_projects_list = $('#github_projects_list');
+        var $github_types_list = $('#github_types_list');
+        var github_types_sel = [];
+        var $github_languages_list = $('#github_languages_list');
+        $github_languages_list.select2();
+
+
+        var $issues_table = $('#issues_table').DataTable( {
+            responsive: true,
+            "pageLength": 20,
+            "bLengthChange": false,
+            "bInfo" : false,
+            "language": {
+                "url": datatables_language_url[current_language]
+            },
+            "ajax": {
+                "url":"/github/issues.json",
+                "dataSrc": ""
+            },
+            "order": [[ 0, "desc" ]],
+            "columns": [
+                { "data": "created_at" , "visible": false},
+                { "data": "type" , "visible": false, "name":"type"},
+                { "data": "language" , "visible": false, "name":"language"},
+                { "data": "project" , "name":"project", "render": function(data, type, row, meta ) {
+                    return data in projects_dictionary ? projects_dictionary[data] : data;
+                }},
+                { "data": "subproject" , "render": function(data, type, row, meta ){
+                    return "<a class='u-textWeight-400' target='_blank' href='"+row.repository_url+"'>"+data+"</a>";
+                    } 
+                },
+                { "data": "title", "render": function(data, type, row, meta ){
+                    return "<a target='_blank' href='"+row.url+"'>"+data+"</a>";
+                    } 
+                },
+                { "data": "labels", "render": function(data, type, row, meta ){
+                    var labelist = '';
+                    data.forEach( function (item){
+                         labelist = labelist + "<span class='labelist u-color-white u-background-40 u-borderRadius-l u-padding-all-xxs u-margin-right-xxs u-margin-left-xxs u-margin-bottom-xxs' >"+item+"</span>";
+                        });
+                    return labelist;
+                    } 
+                },
+            ],
+            initComplete: function () {
+            this.api().column('project:name').every( function () {
+                var column = this;
+                var select = $github_projects_list.on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+                        column.search( val ? val : '', true, false ).draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    var title = d in projects_dictionary ? projects_dictionary[d] : d;
+                    select.append( '<option value="'+title+'">'+title+'</option>' )
+                } );
+            } );
+            this.api().column('language:name').every( function () {
+                var column = this;
+                var select = $github_languages_list.on('change', function () {
+                    var val = $(this).val();
+                    if (val!=null) {
+                        column.search(val.join('|'), true, false, true).draw();
+                    } else {
+                        column.search('', true, false ).draw();
+                    }
+                } );
+                column.data().unique().sort().each( function ( d, j ) {
+                    if (d!=null && d!='') {
+                    select.append( '<option value="'+d+'">'+d+'</option>' );
+                    }
+                } );
+            } );
+            this.api().column('type:name').every( function () {
+                var column = this;
+                column.data().unique().sort().each( function ( d, j ) {
+                    if (d!=null && d!='') {
+                    $github_types_list.append('<span><input type="checkbox" id="github_types_'+j+'" name="github_types_list[]" value="'+d+'"/> '+d+'</span>');
+                    $('#github_types_'+j).on('change', function() {
+                        var val = $(this).val();
+
+                        if (github_types_sel.indexOf(val) > -1) {
+                            github_types_sel.splice( github_types_sel.indexOf(val) ,1);
+                        }
+                        else {
+                            github_types_sel.push(val);
+                        }
+                        
+                        if (github_types_sel.length > 0) {
+                            column.search(github_types_sel.join('|'), true, false, true).draw();
+                        } else {
+                            column.search('', true, false ).draw();
+                        }
+                    });
+
+                    }
+                } );
+
+            } );
+
+            // reset all filters
+            $issues_table_clear.on('click', function (e){
+                $github_types_list.find('input:checked').removeAttr('checked');
+                $github_languages_list.val([]).trigger("change");
+                $github_projects_list.val('');
+                $issues_table.columns().search('', true, false).draw();
+            });
+        }
+        } );
+
+    } );
