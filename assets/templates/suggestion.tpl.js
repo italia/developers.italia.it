fa7544f7f415af0e0aa298aae2bc5071bbd8cfb2
gmereu <gmereu@wellnet.it>
2018-07-12 15:16:00 +0200

    [#158354575] Elasticsearch website api

--- assets/templates/suggestion.tpl.js
+++ assets/templates/suggestion.tpl.js
@@ -7,7 +7,9 @@ templates['suggestion'] = template({"compiler":[7,">= 4.0.0"],"main":function(co
     + alias4(((helper = (helper = helpers.path || (depth0 != null ? depth0.path : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"path","hash":{},"data":data}) : helper)))
     + "\"><span class=\"results\">"
     + ((stack1 = ((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "</span><span class=\"category\">"
+    + "</span><span class=\""
+    + alias4(((helper = (helper = helpers.categoryClass || (depth0 != null ? depth0.categoryClass : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"categoryClass","hash":{},"data":data}) : helper)))
+    + "\">"
     + alias4(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"category","hash":{},"data":data}) : helper)))
     + "</span></a>\n";
 },"useData":true});
