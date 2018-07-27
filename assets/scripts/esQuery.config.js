1b1fc9eb5a3f78396a77de69ebe15eda0fa4e369
Luca Lusso <lussoluca@gmail.com>
2018-07-27 16:42:20 +0200

    Add esQuery.config.js to GIT

--- assets/scripts/esQuery.config.js
+++ assets/scripts/esQuery.config.js
@@ -17,87 +17,87 @@ var pagesQueryconfig = {
   'software_open': {
     'type': ['software'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'reuse_software': {
     'type': ['software'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'administrations': {
     'type': ['software'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'category': {
     'type': ['software'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'all': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'platforms': {
     'type': ['post'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   },
   'api': {
     'type': ['post'],
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
-    },
+      'host': 'http://elasticsearch.developers.italia.it'
+    }
   }
 };
 
 var autocompleteQueryconfig = {
   'all': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
   },
   'platforms': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
   },
   'software_open': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
   },
   'reuse_software': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
   },
   'api': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
   },
   'administrations': {
     'elasticsearch_connection': {
-      'host': 'https://xxx.yyy.www.zzz'
+      'host': 'http://elasticsearch.developers.italia.it'
     },
     'inputSelector': 'form .autocomplete input',
     'size': 9
-  },
-}
+  }
+};
