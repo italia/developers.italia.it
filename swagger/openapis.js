dec4c3cf101dfca7ae100cb9cc16d8fded19c4df
Gianluca Esposito <g@esposi.to>
2018-10-22 11:04:15 +0200

    Update API section

---
+++ swagger/openapis.js
@@ -0,0 +1,9 @@
+// Original content is passed down from
+// https://raw.githubusercontent.com/italia/developers.italia.it/master/_data/apis.yml
+module.exports = [
+  "https://raw.githubusercontent.com/teamdigitale/api-openapi-samples/master/external-apis/geodati.gov.it.yaml",
+  "https://raw.githubusercontent.com/teamdigitale/api-openapi-samples/master/external-apis/api.daf.teamdigitale.it.yaml",
+  "https://raw.githubusercontent.com/teamdigitale/api-openapi-samples/master/external-apis/siopeplus.yaml",
+  "https://raw.githubusercontent.com/teamdigitale/api-openapi-samples/master/external-apis/muoversi2015.e015.servizirl.it.yaml",
+  "https://raw.githubusercontent.com/teamdigitale/api-openapi-samples/master/external-apis/cercaareeprotette.e015.servizirl.it.yaml"
+];
