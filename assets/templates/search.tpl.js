dedbb662b6a3fd03b39c4a50e20d24a8ea5e06c5
Alessandro Ranellucci <alranel@teamdigitale.governo.it>
2019-03-22 21:24:43 +0100

    Bugfix: the software type was not displayed correctly in search results

--- assets/templates/search.tpl.js
+++ assets/templates/search.tpl.js
@@ -4,11 +4,11 @@
     "compiler": [7, ">= 4.0.0"], "main": function (container, depth0, helpers, partials, data) {
       var helper, alias1 = depth0 != null ? depth0 : (container.nullContext || {}), alias2 = helpers.helperMissing, alias3 = "function", alias4 = container.escapeExpression;
 
-      return "<div class=\"col-sm-6 col-md-4 item\">\n  <p class=\""
+      return "<div class=\"col-sm-6 col-md-4 item\">\n  <span class=\""
         + alias4(((helper = (helper = helpers.categoryClass || (depth0 != null ? depth0.categoryClass : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { "name": "categoryClass", "hash": {}, "data": data }) : helper)))
         + "\">"
         + alias4(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { "name": "category", "hash": {}, "data": data }) : helper)))
-        + "</p>\n  <div class=\"main-image\" style=\" background: url("
+        + "</span>\n  <div class=\"main-image\" style=\" background: url("
         + alias4(((helper = (helper = helpers.screenshot || (depth0 != null ? depth0.screenshot : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { "name": "screenshot", "hash": {}, "data": data }) : helper)))
         + ") no-repeat top center;\">\n  </div>\n  <p class=\"name\">"
         + alias4(((helper = (helper = helpers.localisedName || (depth0 != null ? depth0.localisedName : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { "name": "localisedName", "hash": {}, "data": data }) : helper)))
