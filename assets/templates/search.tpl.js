bd7437fb67df7f38cdc3135e568b5dccef64b39c
Alessandro Sebastiani <sebbalex@gmail.com>
2019-11-07 16:14:28 +0100

    Fix wrong tag for icon on search page

--- assets/templates/search.tpl.js
+++ assets/templates/search.tpl.js
@@ -3,18 +3,18 @@
 templates['search'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=container.escapeExpression;
 
-  return "<div class=\"col-sm-6 col-md-4 item\">\n  <p class=\""
+  return "<div class=\"col-sm-6 col-md-4 item\">\n  <span class=\""
     + alias3(((helper = (helper = helpers.categoryClass || (depth0 != null ? depth0.categoryClass : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"categoryClass","hash":{},"data":data}) : helper)))
     + "\">"
     + alias3(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"category","hash":{},"data":data}) : helper)))
-    + "</p>\n  <article class=\"d-flex flex-column align-items-start mb-2 mb-md-5\">\n    <a href=\""
-    + alias3(((helper = (helper = helpers.path || (depth0 != null ? depth0.path : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"path","hash":{},"data":data}) : helper)))
-    + "\" class=\"d-block decoration-none \">\n      <figure>\n        <img class=\"main-image\" src='"
+    + "</span>\n  <div class=\"main-image\" style=\" background: url("
     + alias3(((helper = (helper = helpers.screenshot || (depth0 != null ? depth0.screenshot : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"screenshot","hash":{},"data":data}) : helper)))
-    + "' />\n      </figure>\n      <p class=\"name\">"
+    + ") no-repeat center center;\">\n  </div>\n  <p class=\"name\">"
     + alias3(((helper = (helper = helpers.localisedName || (depth0 != null ? depth0.localisedName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"localisedName","hash":{},"data":data}) : helper)))
-    + "</p>\n      <span class=\"projectslist__listing__url\">"
+    + "</p>\n  <a href=\""
+    + alias3(((helper = (helper = helpers.path || (depth0 != null ? depth0.path : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"path","hash":{},"data":data}) : helper)))
+    + "\"> "
     + alias3(((helper = (helper = helpers.readMore || (depth0 != null ? depth0.readMore : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"readMore","hash":{},"data":data}) : helper)))
-    + " &rarr;</span>\n    </a>\n  </article>\n</div>";
+    + "</a>\n</div>";
 },"useData":true});
 })();
