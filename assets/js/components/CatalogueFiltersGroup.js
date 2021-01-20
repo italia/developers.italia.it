c0f0d9fafb128b1bdcc3275db9462ea373dc94e6
emilioSp <emilio.spatola@gmail.com>
2021-01-20 18:10:15 +0100

    feat: collapsable filters

--- assets/js/components/CatalogueFiltersGroup.js
+++ assets/js/components/CatalogueFiltersGroup.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
-  container: {
+  groupContainer: {
     composes: 'mt-5',
-    maxHeight: '513px',
-    overflow: 'scroll',
+    maxHeight: (showAll) => (showAll ? '90vh' : '360px'),
+    overflow: (showAll) => (showAll ? 'scroll' : 'hidden'),
     paddingLeft: '2px',
+    transition: 'max-height 0.5s',
   },
   checkbox: {
     flexShrink: 0,
@@ -23,18 +24,34 @@ const useStyles = createUseStyles({
     textTransform: 'capitalize',
   },
   title: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     position: 'absolute',
     backgroundColor: 'var(--white)',
-    width: '100%',
+    width: '93%',
+    height: '40px',
     paddingBottom: '8px',
     textTransform: 'uppercase',
     fontWeight: '600',
+  },
+  filtersContainer: {
+    marginTop: '40px',
+  },
+  expandIcon: {
+    composes: 'icon icon-primary',
+    width: '40px',
+    height: '40px',
   },
 });
 
 export const CatalogueFiltersGroup = React.memo(({ title, filters, defaultValues = {}, onChange }) => {
   console.log('CatalogueFiltersGroup');
-  const classes = useStyles();
+  const filtersArray = Object.entries(filters);
+  const [showAll, setShowAll] = useState(false);
+  const container = useRef(null);
+
+  const classes = useStyles(showAll);
   const { register, getValues } = useForm({
     defaultValues,
   });
@@ -42,22 +59,41 @@ export const CatalogueFiltersGroup = React.memo(({ title, filters, defaultValues
     const values = getValues();
     onChange(values);
   };
+
+  const handleToogleShowAll = () => {
+    container.current.scrollTop = 0;
+    setShowAll(!showAll);
+  };
+
   return (
-    <div className={classes.container}>
-      <p className={classes.title}> {title} </p>
-      {Object.entries(filters).map(([key, value]) => (
-        <label key={key} className={classes.label}>
-          <input
-            className={classes.checkbox}
-            type="checkbox"
-            name={key}
-            ref={register}
-            onChange={handleOnChangeFilter}
-          />
-          {value}
-        </label>
-      ))}
-    </div>
+    <>
+      <div className={classes.groupContainer} ref={container}>
+        <div className={classes.title}>
+          <p className="mb-0"> {title} </p>
+          {filtersArray.length > 10 && (
+            <div role="button" onClick={handleToogleShowAll}>
+              <svg className={classes.expandIcon}>
+                <use xlinkHref={`/assets/svg/sprite.svg#${showAll ? 'it-collapse' : 'it-expand'}`}></use>
+              </svg>
+            </div>
+          )}
+        </div>
+        <div className={classes.filtersContainer}>
+          {filtersArray.map(([key, value]) => (
+            <label key={key} className={classes.label}>
+              <input
+                className={classes.checkbox}
+                type="checkbox"
+                name={key}
+                ref={register}
+                onChange={handleOnChangeFilter}
+              />
+              {value}
+            </label>
+          ))}
+        </div>
+      </div>
+    </>
   );
 });
 
