99356c7fdd3e9c59dd1b0cd2d8328625aa47ae84
emilioSp <emilio.spatola@gmail.com>
2021-02-09 17:18:54 +0100

    chore: custom elements should have a dash in their name

--- assets/js/App.js
+++ assets/js/App.js
@@ -6,8 +6,8 @@ import { CatalogueContainer } from './components/Catalogue/CatalogueContainer.js
 export const App = () => {
   const search = useRef([]);
   const catalogue = useRef([]);
-  search.current = Array.from(document.getElementsByTagName('search'));
-  catalogue.current = Array.from(document.getElementsByTagName('catalogue'));
+  search.current = Array.from(document.getElementsByTagName('custom-search'));
+  catalogue.current = Array.from(document.getElementsByTagName('custom-catalogue'));
   return (
     <>
       {search.current[0] && ReactDOM.createPortal(<SearchContainer />, search.current[0])}
