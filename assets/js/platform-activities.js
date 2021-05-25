45d069e59ed6da111b05242d40e91028fcfeb235
Andrea Stagi <stagi.andrea@gmail.com>
2021-05-25 16:53:01 +0200

    fix: lint errors

--- assets/js/platform-activities.js
+++ assets/js/platform-activities.js
@@ -1,15 +1,15 @@
-let platforms = document.querySelectorAll(".projectslist__listing__activities-counter")
+const platforms = document.querySelectorAll('.projectslist__listing__activities-counter');
 
 if (platforms.length > 0) {
-  fetch("/assets/github_issues.json")
-    .then(response => response.json())
-    .then(data => {
-      for (let i = 0 ; i < data.length ; i++) {
-        let selected = document.querySelector(`#projectslist-activity-counter-${data[i].project}`)
+  fetch('/assets/github_issues.json')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const selected = document.querySelector(`#projectslist-activity-counter-${data[i].project}`);
         if (selected) {
-          let counter = parseInt(selected.innerHTML)
-          counter++
-          selected.innerHTML = `${counter}`
+          let counter = parseInt(selected.innerHTML, 10);
+          counter++;
+          selected.innerHTML = `${counter}`;
         }
       }
     });
