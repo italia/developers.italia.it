57bc9312084d4bf3092febbad35acbbda34e10e7
smonolo <lu3mtex@gmail.com>
2024-04-21 15:29:15 +0200

    feat(next): create base next app

---
+++ next/tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  plugins: []
+}
+
+export default config
