55e731cd3a4b6dd67e3f98a0c998c5b774c54645
Fabio Bonelli <fb@fabiobonelli.it>
2024-07-30 11:05:43 +0200

    lint

--- assets/js/components/PubliccodeBadge.js
+++ assets/js/components/PubliccodeBadge.js
@@ -4,28 +4,31 @@ import PropTypes from 'prop-types';
 import { Spinner } from './Spinner';
 
 const stateClass = {
-  'loading': 'secondary',
-  'good': 'success',
-  'error': 'danger',
+  loading: 'secondary',
+  good: 'success',
+  error: 'danger',
 };
 
 export const PubliccodeBadge = React.memo(({ id }) => {
   const [publiccodeState, setPubliccodeState] = useState('loading');
+  const logUrl = `https://api.developers.italia.it/v1/software/${id}/logs`;
 
-  useEffect(async () => {
-    const res = await fetch(logUrl);
+  useEffect(() => {
+    async function fetchLogs() {
+      const res = await fetch(logUrl);
 
-    if (res.status >= 200 || res.status <= 299) {
-      const data = await res.json();
-      const good = data.data[0]?.message.includes('GOOD publiccode.yml');
+      if (res.status >= 200 || res.status <= 299) {
+        const data = await res.json();
+        const good = data.data[0]?.message.includes('GOOD publiccode.yml');
 
-      setPubliccodeState(good ? 'good' : 'error');
-    } else {
-      setPubliccodeState('error');
+        setPubliccodeState(good ? 'good' : 'error');
+      } else {
+        setPubliccodeState('error');
+      }
     }
-  }, [logUrl]);
 
-  const logUrl = `https://api.developers.italia.it/v1/software/${id}/logs`
+    fetchLogs();
+  }, [logUrl]);
 
   return (
     <>
@@ -41,6 +44,7 @@ export const PubliccodeBadge = React.memo(({ id }) => {
         href={logUrl}
         target="_blank"
         aria-label="Log results of the latest publiccode.yml crawling"
+        rel="noreferrer"
       >
         (log)
       </a>
--- assets/js/components/Spinner.js
+++ assets/js/components/Spinner.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './Spinner.css';
 
-export const Spinner = React.memo(() => {
-  return (
-    <>
-      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-    </>
-  );
-});
+export const Spinner = React.memo(() => (
+  <>
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </>
+));
 
 Spinner.displayName = 'Spinner';
