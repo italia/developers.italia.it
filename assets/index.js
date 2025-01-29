1d99eccce3dca587cf226346a1da229e172c4f60
Andrea Stagi <stagi.andrea@gmail.com>
2025-01-29 16:27:20 +0100

    fix: restore back to top

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 // This is Webpack's entrypoint.
 
-import 'bootstrap-italia';
+import {BackToTop} from 'bootstrap-italia';
 
 import './js/logos';
 import './js/main';
@@ -16,6 +16,13 @@ import './scss/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './js/App.js';
+
+// Init Bootstrap Italia JS
+
+new BackToTop(document.getElementsByClassName('back-to-top')[0])
+
+
+// Init React components
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
