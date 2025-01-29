91d9ef6620d64dbe4ca60b42adee4a2aeeb6fa23
Andrea Stagi <stagi.andrea@gmail.com>
2025-01-29 11:03:37 +0100

    fix: use createRoot in react

--- assets/index.js
+++ assets/index.js
@@ -17,9 +17,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './js/App.js';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
