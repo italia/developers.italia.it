dc8253c1c81cc19948cce7e2786d053120e141b7
Andrea Stagi <stagi.andrea@gmail.com>
2025-01-29 16:39:52 +0100

    feat: use only selected elements from bsi

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 // This is Webpack's entrypoint.
 
-import {BackToTop} from 'bootstrap-italia';
+import { BackToTop, Dropdown, Tab, Collapse } from 'bootstrap-italia';
 
 import './js/logos';
 import './js/main';
@@ -19,8 +19,22 @@ import { App } from './js/App.js';
 
 // Init Bootstrap Italia JS
 
-new BackToTop(document.getElementsByClassName('back-to-top')[0])
+BackToTop.getOrCreateInstance(document.getElementsByClassName('back-to-top')[0]);
 
+const dropdownElements = document.querySelectorAll("[data-bs-toggle='dropdown']");
+for (const element of dropdownElements) {
+  Dropdown.getOrCreateInstance(element);
+}
+
+const tabElements = document.querySelectorAll("[data-bs-toggle='tab']");
+for (const element of tabElements) {
+  Tab.getOrCreateInstance(element);
+}
+
+const collapseElements = document.querySelectorAll("[data-bs-toggle='collapse']");
+for (const element of collapseElements) {
+  Collapse.getOrCreateInstance(element);
+}
 
 // Init React components
 
